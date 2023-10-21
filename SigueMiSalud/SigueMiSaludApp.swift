6c120054d46da6107343bddd4f9429c316bcaab0
Nick Sillik <nick@sillik.org>
2023-10-21 11:06:49 -0400

    Initial Commit

---
+++ SigueMiSalud/SigueMiSaludApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigueMiSaludApp.swift
+//  SigueMiSalud
+//
+//  Created by Nick Sillik on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SigueMiSaludApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
